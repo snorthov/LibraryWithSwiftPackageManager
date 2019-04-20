67497fbbfe0d3a5644990069be3bbbdb64b1bbe2
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2019-04-20 12:36:33 +1000

    Update to swift tools 4.0

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
-    exclude: ["DirectoryToExclude"]
+    exclude: ["DirectoryToExclude"],
+    targets: [
+        .target(
+            name: "MyLibrary",
+            dependencies: [],
+            path: "Sources")
+    ]
 )
